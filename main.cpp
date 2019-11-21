44266f7b3f21ff15202d926194c866c187131021
William Longpre <williamlongpre@hotmail.com>
2019-11-20 19:34:43 -0500

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file main.cpp
+ * 
+ * @author William Longpré, Étienne Bellerose
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-20
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+/*----------Includes----------*/
+#include <math.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <iostream>
+
+/*----------Defines-----------*/
+
+
+/*----------Usings------------*/
+using namespace std;
+
+/*---------Variables----------*/
+
+
+/*---------Prototypes---------*/
+void Routine(void);
+void init(void);
+
+int main(int argv, char** argc)
+{
+    //Initialization
+    init();
+    //Execution loop
+    Routine();
+}
+
+void init()
+{
+    /*-------Initialization-------*/
+
+}
+
+void Routine()
+{
+    while(true)
+    {
+
+    }
+}
